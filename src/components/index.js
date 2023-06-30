ca8c87c52ba6e0faa141f401aad20bf25e94588f
skp544 <shubhamkumarprajapati544@gmail.com>
2023-07-01 00:59:51 +0530

    first commit - files and folder structure

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SliderComponent from "./Common/SliderComponent";
+import SliderSelect from "./SliderSelect";
+import Navbar from "./Navbar";
+import Result from "./Result";
+import TenureSelect from "./TenureSelect";
+
+export { SliderComponent, SliderSelect, Navbar, Result, TenureSelect };
